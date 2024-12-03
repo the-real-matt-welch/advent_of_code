ee9ce304f67cd11add13b5523b49ec168135f9db
the-real-matt-welch <mmwelch19@gmail.com>
2024-12-03 16:38:51 +1000

    make day 3 regex technically correct (the problem didn't matter)

--- src/solutions_2024/day03.rs
+++ src/solutions_2024/day03.rs
@@ -3,11 +3,11 @@ use std::fmt::Debug;
 
 pub fn part1<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
     let text = input.join("");
-    let re = Regex::new("mul\\(\\d+,\\d+\\)").unwrap();
+    let re = Regex::new("mul\\(\\d{1,3},\\d{1,3}\\)").unwrap();
     let instructions: Vec<&str> =
         re.find_iter(&text).map(|m| m.as_str()).collect();
 
-    let number = Regex::new("\\d+").unwrap();
+    let number = Regex::new("\\d{1,3}").unwrap();
     let mut total = 0;
     for instruction in &instructions {
         let numbers: Vec<u32> = number
@@ -23,11 +23,12 @@ pub fn part1<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
 
 pub fn part2<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
     let text = input.join("");
-    let re = Regex::new("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)").unwrap();
+    let re =
+        Regex::new("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)").unwrap();
     let instructions: Vec<&str> =
         re.find_iter(&text).map(|m| m.as_str()).collect();
 
-    let number = Regex::new("\\d+").unwrap();
+    let number = Regex::new("\\d{1,3}").unwrap();
     let mut total = 0;
     let mut switch = true;
     for instruction in &instructions {
