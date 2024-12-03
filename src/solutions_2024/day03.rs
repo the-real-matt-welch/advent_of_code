ae7752040589b021b93cc6360e2c7eebd0c6a4dd
the-real-matt-welch <mmwelch19@gmail.com>
2024-12-03 21:43:51 +1000

    why was I collecting iterators just to loop over them???

--- src/solutions_2024/day03.rs
+++ src/solutions_2024/day03.rs
@@ -3,19 +3,16 @@ use std::fmt::Debug;
 
 pub fn part1<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
     let text = input.join("");
-    let re = Regex::new("mul\\(\\d{1,3},\\d{1,3}\\)").unwrap();
-    let instructions: Vec<&str> =
-        re.find_iter(&text).map(|m| m.as_str()).collect();
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let instructions = re.find_iter(&text).map(|m| m.as_str());
 
-    let number = Regex::new("\\d{1,3}").unwrap();
+    let number = Regex::new(r"\d{1,3}").unwrap();
     let mut total = 0;
-    for instruction in &instructions {
-        let numbers: Vec<u32> = number
+    for instruction in instructions {
+        total += number
             .find_iter(instruction)
-            .map(|m| m.as_str().parse().unwrap())
-            .collect();
-        let (first, second) = (numbers[0], numbers[1]);
-        total += first * second;
+            .map(|m| m.as_str().parse::<u32>().unwrap())
+            .product::<u32>();
     }
 
     total
@@ -23,25 +20,21 @@ pub fn part1<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
 
 pub fn part2<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
     let text = input.join("");
-    let re =
-        Regex::new("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)").unwrap();
-    let instructions: Vec<&str> =
-        re.find_iter(&text).map(|m| m.as_str()).collect();
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)").unwrap();
+    let instructions = re.find_iter(&text).map(|m| m.as_str());
 
-    let number = Regex::new("\\d{1,3}").unwrap();
+    let number = Regex::new(r"\d{1,3}").unwrap();
     let mut total = 0;
     let mut switch = true;
-    for instruction in &instructions {
-        match *instruction {
+    for instruction in instructions {
+        match instruction {
             "do()" => switch = true,
             "don't()" => switch = false,
             _ => {
-                let numbers: Vec<u32> = number
+                total += number
                     .find_iter(instruction)
-                    .map(|m| m.as_str().parse().unwrap())
-                    .collect();
-                let (first, second) = (numbers[0], numbers[1]);
-                total += first * second * (switch as u32);
+                    .map(|m| m.as_str().parse::<u32>().unwrap())
+                    .fold(switch as u32, |a, b| a * b);
             }
         }
     }
