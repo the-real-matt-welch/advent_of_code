8d42e7a896a27e8fd081652e24732004d1d7476f
the-real-matt-welch <mmwelch19@gmail.com>
2024-12-04 11:04:51 +1000

    tiny download script

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+source .env
+set -l day $argv[1]
+
+curl -H "Cookie: session=$aoc_cookie" https://adventofcode.com/2024/day/$day/input > inputs/2024/day(string pad -w2 -c0 $day).txt
