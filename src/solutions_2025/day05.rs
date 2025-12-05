b79ddddefdde3a42b87035812380ea73dda1aa7e
the-real-matt-welch <mmwelch19@gmail.com>
2025-12-05 19:11:48 +1000

    day 5

--- src/solutions_2025/day05.rs
+++ src/solutions_2025/day05.rs
@@ -1,9 +1,44 @@
+use range_union_find::RangeUnionFind;
 use std::fmt::Debug;
+use std::ops::RangeInclusive;
 
 pub fn part1<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
-    "Not implemented"
+    let blank = input.iter().position(|s| s.is_empty()).unwrap();
+    let (fresh, ids) = input.split_at(blank);
+    let ids = &ids[1..];
+
+    let fresh: Vec<RangeInclusive<u64>> = fresh
+        .iter()
+        .map(|x| {
+            x.split_once('-')
+                .map(|(a, b)| a.parse::<u64>().unwrap()..=b.parse().unwrap())
+                .unwrap()
+        })
+        .collect();
+    ids.iter()
+        .map(|n| n.parse::<u64>().unwrap())
+        .filter(|n| fresh.iter().any(|r| r.contains(n)))
+        .count()
 }
 
 pub fn part2<'a>(input: &'a Vec<&'a str>) -> impl Debug + 'a {
-    "Not implemented"
+    let blank = input.iter().position(|s| s.is_empty()).unwrap();
+    let (fresh, ids) = input.split_at(blank);
+
+    let mut ranges = RangeUnionFind::new();
+    let fresh = fresh
+        .iter()
+        .map(|x| {
+            x.split_once('-')
+                .map(|(a, b)| a.parse::<u64>().unwrap()..=b.parse().unwrap())
+                .unwrap()
+        })
+        .for_each(|r| {
+            let _ = ranges.insert_range(&r);
+        });
+    ranges
+        .to_collection::<Vec<RangeInclusive<u64>>>()
+        .into_iter()
+        .map(|r| r.count())
+        .sum::<usize>()
 }
